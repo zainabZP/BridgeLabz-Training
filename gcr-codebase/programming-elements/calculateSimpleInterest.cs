96ce51ef2e2592caf8934a3bda2cb55be8fb5afd
zainabZP <122631115+zainabZP@users.noreply.github.com>
2025-12-18 20:43:18 +0530

    [Zainab Parveen] Calculate Simple Interest

--- gcr-codebase/programming-elements/calculateSimpleInterest.cs
+++ gcr-codebase/programming-elements/calculateSimpleInterest.cs
@@ -4,11 +4,12 @@ class calculateSimpleInterest
 {
     static void Main()
     {
+        // taking input from user
         string[] inputs = Console.ReadLine().Split();
-        int p = int.Parse(inputs[0]);
-        int r = int.Parse(inputs[1]);
-        int t = int.Parse(inputs[2]);
-        double SI = (p * r * t) / 100.0;
-        Console.WriteLine(SI);
+        int p = int.Parse(inputs[0]); // taking principle as input
+        int r = int.Parse(inputs[1]); // taking rate as input
+        int t = int.Parse(inputs[2]); // taking time as input
+        double SI = (p * r * t) / 100.0; // calculating simple interest
+        Console.WriteLine(SI); // printing simple interest
     }
 }
