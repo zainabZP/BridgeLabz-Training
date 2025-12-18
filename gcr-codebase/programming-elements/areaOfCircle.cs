d60631996e2bc3ff2ffabd3f90dc4337dc88d7ec
zainabZP <122631115+zainabZP@users.noreply.github.com>
2025-12-18 20:39:26 +0530

    [Zainab Parveen] Area Of Circle

--- gcr-codebase/programming-elements/areaOfCircle.cs
+++ gcr-codebase/programming-elements/areaOfCircle.cs
@@ -4,8 +4,8 @@ class areaOfCircle
 {
     static void Main()
     {
-        int radius = int.Parse(Console.ReadLine());
-        double area = 3.14 * (radius * radius);
-        Console.WriteLine(area);
+        int radius = int.Parse(Console.ReadLine()); // taking radius as input from user
+        double area = 3.14 * (radius * radius); // calculating area of circle
+        Console.WriteLine(area); // printing area of circle
     }
 }
