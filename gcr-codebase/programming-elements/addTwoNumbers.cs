11421c1f0dd4378d33125874bf6852e0f82fb814
zainabZP <122631115+zainabZP@users.noreply.github.com>
2025-12-18 20:37:54 +0530

    [Zainab Parveen] Add Two Numbers

--- gcr-codebase/programming-elements/addTwoNumbers.cs
+++ gcr-codebase/programming-elements/addTwoNumbers.cs
@@ -4,10 +4,11 @@ class addTwoNumbers
 {
     static void Main()
     {
+        // taking input of two numbers from user
         string[] inputs = Console.ReadLine().Split();
-        int a = int.Parse(inputs[0]);
-        int b = int.Parse(inputs[1]);
-        int c = a + b;
-        Console.WriteLine(c);
+        int a = int.Parse(inputs[0]);  // taking first number input
+        int b = int.Parse(inputs[1]); // taking second number input
+        int c = a + b; // calculating sum
+        Console.WriteLine(c); // printing sum of two numbers
     }
 }
