b1066851f8e25af7fcbd6165c7dc75a09c381191
zainabZP <122631115+zainabZP@users.noreply.github.com>
2025-12-18 20:44:56 +0530

    [Zainab Parveen] Celcius To Fahrenheit

--- gcr-codebase/programming-elements/celciusToFahrenheit.cs
+++ gcr-codebase/programming-elements/celciusToFahrenheit.cs
@@ -4,8 +4,8 @@ class celciusToFahrenheit
 {
     static void Main()
     {
-        int temp = int.Parse(Console.ReadLine());
-        int f = (temp * 9 / 5) + 32;
-        Console.WriteLine(f);
+        int temp = int.Parse(Console.ReadLine()); // taking temperature as input
+        int f = (temp * 9 / 5) + 32; // converting cecius to fahrenheit
+        Console.WriteLine(f); // printing temperature in fahrenheit
     }
 }
