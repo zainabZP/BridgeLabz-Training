25ace543cf0129de12cb37c2cc838dfcd964c433
zainabZP <122631115+zainabZP@users.noreply.github.com>
2025-12-18 20:49:46 +0530

    [Zainab Parveen] Power Calculation Of a Number

--- gcr-codebase/programming-elements/powerCalculation.cs
+++ gcr-codebase/programming-elements/powerCalculation.cs
@@ -5,9 +5,9 @@ class powerCalculation
     static void Main()
     {
         string[] inputs = Console.ReadLine().Split();
-        double e = double.Parse(inputs[0]);
-        double b = double.Parse(inputs[1]);
-        double result = Math.Pow(b, e);
-        Console.WriteLine(result);
+        double e = double.Parse(inputs[0]); // taking power as input
+        double b = double.Parse(inputs[1]); // taking base as input
+        double result = Math.Pow(b, e); // calculating base to the power e
+        Console.WriteLine(result); // printing calculated result
     }
 }
