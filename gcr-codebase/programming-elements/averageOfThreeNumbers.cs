c4000501ca3ddc1946dc2fac4f6e4a7ae933eb00
zainabZP <122631115+zainabZP@users.noreply.github.com>
2025-12-18 20:41:26 +0530

    [Zainab Parveen] Average Of Three Numbers

--- gcr-codebase/programming-elements/averageOfThreeNumbers.cs
+++ gcr-codebase/programming-elements/averageOfThreeNumbers.cs
@@ -4,11 +4,12 @@ class averageOfThreeNumbers
 {
     static void Main()
     {
+        // taking input from user
         string[] inputs = Console.ReadLine().Split();
-        int a = int.Parse(inputs[0]);
-        int b = int.Parse(inputs[1]);
-        int c = int.Parse(inputs[2]);
-        double avg = (a + b + c) / 3.0;
-        Console.WriteLine(avg);
+        int a = int.Parse(inputs[0]); // first number input
+        int b = int.Parse(inputs[1]); // second number input
+        int c = int.Parse(inputs[2]); // third number input
+        double avg = (a + b + c) / 3.0; // calculating average
+        Console.WriteLine(avg); // printing average
     }
 }
