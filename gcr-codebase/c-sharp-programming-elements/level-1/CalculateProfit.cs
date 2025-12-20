c6377eabd2f68642d3ba2495d5dd727cb7551227
zainabZP <122631115+zainabZP@users.noreply.github.com>
2025-12-20 21:15:06 +0530

    [Zainab Parveen] refactored. Calculate Profit

--- gcr-codebase/c-sharp-programming-elements/level-1/CalculateProfit.cs
+++ gcr-codebase/c-sharp-programming-elements/level-1/CalculateProfit.cs
@@ -4,9 +4,13 @@ class CalculateProfit
 {
     static void Main()
     {
+		// Creating an int variable 'costPrice' and assign value 129
         double costPrice = 129;
+		// Creating an int variable 'sellingPrice' and assign value 191
         double sellingPrice = 191;
+		// calculating profit percentage
 		double profitPercent=((sellingPrice-costPrice)/costPrice)*100;
+		// printing profit percentage
         Console.WriteLine(profitPercent);
     }
 }
