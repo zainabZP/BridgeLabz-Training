55d6bf28dcc3a188fc6bcce7c1bff5a30bca8a29
zainabZP <122631115+zainabZP@users.noreply.github.com>
2025-12-20 21:56:07 +0530

    [Zainab Parveen] refactored. Calculate Volume Of Earth

--- gcr-codebase/c-sharp-programming-elements/level-1/VolumeOfEarth.cs
+++ gcr-codebase/c-sharp-programming-elements/level-1/VolumeOfEarth.cs
@@ -4,9 +4,12 @@ class VolumeOfEarth
 {
     static void Main()
     {
+		// create double varible 'earthRadius' and assign value 6378
         double earthRadius = 6378;
         double constant=4.0/3.0;
+		// calculating volume of earth
 		double volume=constant*3.14*earthRadius*earthRadius;
+		// printing volume of earth
 		Console.WriteLine(volume);
     }
 }
