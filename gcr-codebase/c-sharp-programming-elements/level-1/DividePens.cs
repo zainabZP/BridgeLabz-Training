87f7f87a62e0212cdc5df954450ec6c86b7c5183
zainabZP <122631115+zainabZP@users.noreply.github.com>
2025-12-20 21:36:30 +0530

    [Zainab Parveen] refactored. calculate pens per child and pens left undivided

--- gcr-codebase/c-sharp-programming-elements/level-1/DividePens.cs
+++ gcr-codebase/c-sharp-programming-elements/level-1/DividePens.cs
@@ -4,10 +4,15 @@ class DividePens
 {
     static void Main()
     {
+		// Create a int variable 'totalPens' and assign value 14.
         int totalPens = 14;
+		// divide pens among 3 children
         int pensForEach = totalPens/3;
+		// calculating remaining pens
 		int remainingPens=totalPens%3;
+		// printing pens per child
         Console.WriteLine("Pens each student gets="+pensForEach);
+		// printing pens left undivided
 		Console.WriteLine("Pens left undivided="+remainingPens);
     }
 }
