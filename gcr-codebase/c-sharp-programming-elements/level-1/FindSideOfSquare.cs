c5d31a0da0ebd7063dcc2f93956339a1f32236af
zainabZP <122631115+zainabZP@users.noreply.github.com>
2025-12-20 21:40:56 +0530

    [Zainab Parveen] refactored. Find Side Of Square

--- gcr-codebase/c-sharp-programming-elements/level-1/FindSideOfSquare.cs
+++ gcr-codebase/c-sharp-programming-elements/level-1/FindSideOfSquare.cs
@@ -4,9 +4,13 @@ class FindSideOfSquare
 {
     static void Main()
     {
+        // taking perimeter as input from user
+        Console.Write("Enter perimeter: ");
         string[] input = Console.ReadLine().Split();
         double perimeter = double.Parse(input[0]);
+        // calculating length of one side of square
         double side =perimeter/4.0;
-        Console.WriteLine(side);
+        // printing length of one side
+        Console.WriteLine("one side = "+side);
     }
 }
