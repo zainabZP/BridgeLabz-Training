e027b80eb74f6bcefe45579a2be68efe87ced278
zainabZP <122631115+zainabZP@users.noreply.github.com>
2025-12-20 21:29:17 +0530

    [Zainab Parveen] refactored. Convert Kilometers To Miles

--- gcr-codebase/c-sharp-programming-elements/level-1/ConvertKilometersToMiles.cs
+++ gcr-codebase/c-sharp-programming-elements/level-1/ConvertKilometersToMiles.cs
@@ -4,8 +4,11 @@ class ConvertKilometersToMiles
 {
     static void Main()
     {
+        // Create a double variable 'kilometers' and assign value 10.8
         double kilometers = 10.8;
+        // converting km to miles
         double miles = kilometers * 1.6;
+        // printing distance after conversion
         Console.WriteLine(miles);
     }
 }
