56f2a1196676acc2442d2adf958d138d7c9465f5
zainabZP <122631115+zainabZP@users.noreply.github.com>
2025-12-20 22:02:51 +0530

    [Zainab Parveen] refactored. Convert Weight From Pounds To Kilograms

--- gcr-codebase/c-sharp-programming-elements/level-2/ConvertPoundsToKilograms.cs
+++ gcr-codebase/c-sharp-programming-elements/level-2/ConvertPoundsToKilograms.cs
@@ -3,11 +3,14 @@ using System;
 class ConvertPoundsToKilograms{
 
     static void Main(){
-	
+
+		// take weight in pounds as input from use
         Console.Write("Enter weight in pounds =");
         String[] inp = Console.ReadLine().Split();
         double pounds = Convert.ToDouble(inp[0]);
+		// convert pounds to kilogram
         double kg = pounds / 2.2;
+		// print converted weight in kg
         Console.WriteLine("The weight in kilograms = " + kg);
     }
 }
