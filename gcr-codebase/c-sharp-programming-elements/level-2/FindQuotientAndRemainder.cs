c51c293952437d8f99192ad18710faf787283adb
zainabZP <122631115+zainabZP@users.noreply.github.com>
2025-12-20 22:25:13 +0530

    [Zainab Parveen] refactored. Find Quotient And Remainder

--- gcr-codebase/c-sharp-programming-elements/level-2/FindQuotientAndRemainder.cs
+++ gcr-codebase/c-sharp-programming-elements/level-2/FindQuotientAndRemainder.cs
@@ -1,12 +1,16 @@
 using System;
 class FindQuotientAndRemainder {
     public static void Main() {
+        // take two numbers as user input
         Console.Write("Enter first number: ");
         int first = Convert.ToInt32(Console.ReadLine());
         Console.Write("Enter the second number: ");
         int second = Convert.ToInt32(Console.ReadLine());
+        // calculating quotient
         int q = first / second;
+        // calculating remainder
         int r = first % second;
+        // printing quotient and remainder
         Console.WriteLine("Quotient = " + q + " and Remainder = " + r);
     }
 }
