f112e471718959674a53be2ae361a9a81772aad5
zainabZP <122631115+zainabZP@users.noreply.github.com>
2025-12-20 22:18:29 +0530

    Update DivideChocolates.cs

--- gcr-codebase/c-sharp-programming-elements/level-2/DivideChocolates.cs
+++ gcr-codebase/c-sharp-programming-elements/level-2/DivideChocolates.cs
@@ -1,13 +1,1 @@
-using System;
 
-class DivideChocolates{
-    static void Main(){
-        Console.Write("Enter Total Chocolates and number of Children : ");
-        string[] inp = Console.ReadLine().Split();
-        int chocolates = int.Parse(inp[0]);
-        int children = int.Parse(inp[1]);
-        int chocolatesForEachChild = chocolates/children;
-        int chocolatesLeftUndivided = chocolates%children;
-        Console.WriteLine("Chocolates per child = " + chocolatesForEachChild +" and the number chocolates left undivided = " + chocolatesLeftUndivided);
-    }
-}
