81c31513010f8ce11f736166cb24e8d7205e0a5b
zainabZP <122631115+zainabZP@users.noreply.github.com>
2025-12-20 21:21:27 +0530

    [Zainab Parveen] refactored. Basic Calculator

--- gcr-codebase/c-sharp-programming-elements/level-1/Calculator.cs
+++ gcr-codebase/c-sharp-programming-elements/level-1/Calculator.cs
@@ -3,14 +3,15 @@ using System;
 class Calculator{
 
 	static void Main(){
-	
+
+		// taking two numbers asuser input to perform basic operations of a calculator
 		Console.Write("Enter first number: "); 
         double a = Convert.ToDouble(Console.ReadLine());
         Console.Write("Enter second number: ");
         double b = Convert.ToDouble(Console.ReadLine());
-		Console.WriteLine("sum ="  + (a+b) );
-		Console.WriteLine("subtraction =" + (a-b));
-		Console.WriteLine("Multiplication =" + (a*b));
-		Console.WriteLine("Division =" + (a/b));
+		Console.WriteLine("sum ="  + (a+b) ); // calculating and printing addition of two numbers
+		Console.WriteLine("subtraction =" + (a-b)); // calculating and printing subtraction of two numbers
+		Console.WriteLine("Multiplication =" + (a*b)); // calculating and printing multiplication of two numbers
+		Console.WriteLine("Division =" + (a/b)); // calculating and printing division of two numbers
 	}
 }
