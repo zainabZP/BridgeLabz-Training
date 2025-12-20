5918453e31ef22cb95d0c1a29b0bbdca36b27669
zainabZP <122631115+zainabZP@users.noreply.github.com>
2025-12-20 22:26:50 +0530

    [Zainab Parveen] refactored. Int Operations

--- gcr-codebase/c-sharp-programming-elements/level-2/IntOperation.cs
+++ gcr-codebase/c-sharp-programming-elements/level-2/IntOperation.cs
@@ -1,16 +1,19 @@
 using System;
 class IntOperation{
     static void Main() {
+        // take 3 integers as input from user
         Console.Write("Enter value of x: ");
         int x= Convert.ToInt32(Console.ReadLine());
         Console.Write("Enter value of y: ");
         int y = Convert.ToInt32(Console.ReadLine());
         Console.Write("Enter value of z: ");
         int z = Convert.ToInt32(Console.ReadLine());
+        // performing diffetent operations on input numbers
         int firstResult = (x+y*z);
         int secondResult = x*y+z;
         int third = (z+x/y);
         int fourth = (x%y+z);
+        // printing result of varius operations
         Console.WriteLine($"x+y*z = {firstResult}, x*y+z = {secondResult}, z+x/y = {third}, x%y+z = {fourth}");
     }
 }
