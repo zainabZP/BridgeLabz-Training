56964df54ac6485a7a24ca5a2e428eeb36403222
zainabZP <122631115+zainabZP@users.noreply.github.com>
2025-12-20 21:58:05 +0530

    [Zainab Parveen] refactored. Calculate Simple Interest

--- gcr-codebase/c-sharp-programming-elements/level-2/CalculateSimpleInterest.cs
+++ gcr-codebase/c-sharp-programming-elements/level-2/CalculateSimpleInterest.cs
@@ -1,12 +1,15 @@
 using System;
 class CalculateSimpleInterest{
     static void Main(){
+        // taking principal, rate and time as user input
         Console.Write("Enter principal, rate and time: ");
         string[] inp=Console.ReadLine().Split();
         double p=Convert.ToDouble(inp[0]);
         double r=Convert.ToDouble(inp[1]);
         double t=Convert.ToDouble(inp[2]);
+        // calculating simple interes
         double simpleInterest=(p*r*t)/100;
+        // printing calculated SI
         Console.WriteLine("Simple Interest = " + simpleInterest);
     }
 }
