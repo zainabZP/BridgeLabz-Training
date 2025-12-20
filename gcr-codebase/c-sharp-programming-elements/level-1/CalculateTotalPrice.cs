8ab3dc8929b0ed33f59c024e28db8cae2bbf5a2d
zainabZP <122631115+zainabZP@users.noreply.github.com>
2025-12-20 21:18:37 +0530

    [Zainab Parveen] refactored. Calculate Total Price

--- gcr-codebase/c-sharp-programming-elements/level-1/CalculateTotalPrice.cs
+++ gcr-codebase/c-sharp-programming-elements/level-1/CalculateTotalPrice.cs
@@ -4,10 +4,13 @@ class CalculateTotalPrice
 {
     static void Main()
     {
+        // taking user input for unit price and quantity of product.
         string[] inputs = Console.ReadLine().Split();
         int unitPrice = int.Parse(inputs[0]);
         int quantity = int.Parse(inputs[1]);
+        // calculating total bill
         int totalPrice = unitPrice*quantity;
+        // printing bill
         Console.WriteLine(totalPrice);
     }
 }
