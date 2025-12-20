49487e52daa85497ddfe995dee6194c56bf3b5b2
zainabZP <122631115+zainabZP@users.noreply.github.com>
2025-12-20 22:05:25 +0530

    [Zainab Parveen] refactored. Distribute Chocolates

--- gcr-codebase/c-sharp-programming-elements/level-2/DistributeChocolates.cs
+++ gcr-codebase/c-sharp-programming-elements/level-2/DistributeChocolates.cs
@@ -3,12 +3,16 @@ class DistributeChocolates{
 	
     public static void Main(){
 
+		// Take total chocolates and number of children as inputs from user
         Console.Write("Enter Total Chocolates and Children : ");
         string[] inputs = Console.ReadLine().Split();
         int chocolates = int.Parse(inputs[0]);
         int children = int.Parse(inputs[1]);
+		// calculate chocolates per child
         int chocolatesPerChild=(chocolates/children);
+		// calculate chocolates left undivided
         int chocolatesLeft = (chocolates % children);
+		// print chocolates per child and chocolates left undivided
         Console.WriteLine("chocolates Per Child = " + chocolatesPerChild);
 		Console.WriteLine("chocolates left = " + chocolatesLeft);
     }
