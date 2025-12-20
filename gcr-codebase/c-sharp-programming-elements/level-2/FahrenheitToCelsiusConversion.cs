eeac7fb103223f2206a23fa6b29de4b0ecd525fa
zainabZP <122631115+zainabZP@users.noreply.github.com>
2025-12-20 22:23:19 +0530

    [Zainab Parveen] refactored. Fahrenheit To Celsius Conversion

--- gcr-codebase/c-sharp-programming-elements/level-2/FahrenheitToCelsiusConversion.cs
+++ gcr-codebase/c-sharp-programming-elements/level-2/FahrenheitToCelsiusConversion.cs
@@ -1,9 +1,12 @@
 using System;
 class FahrenheitToCelsiusConversion {
     static void Main() {
+        // take temperature in fahrenheit as user input
         Console.Write("Enter temperature in Fahrenheit: ");
         double f = Convert.ToDouble(Console.ReadLine());
+        // convert temperature to celcius
         double c =(f-32)*5.0/9.0;
+        // print temperature in celcius
         Console.WriteLine("celcius = "+celcius);
     }
 }
