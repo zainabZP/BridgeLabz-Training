72336cfa5be9017702b3f2a82257ac59f8eb5975
Zainab parveen <zainab.parveen_cs22@gla.ac.in>
2025-12-22 22:04:23 +0530

    [Zainab Parveen] calculate bonus of employee

---
+++ gcr-codebase/control-flow/level-1/EmployeeBonus.cs
@@ -0,0 +1,17 @@
+using System;
+class BonusOfEmployees{	
+    static void Main(){
+		// take salary and year as input from user
+        double salary = double.Parse(Console.ReadLine());
+        int y = int.Parse(Console.ReadLine());
+		// if year > 5, bonus will be 5% of salary
+		if (y>5) {
+            double bonus = salary * 0.05;
+            Console.WriteLine("Bonus = " + bonus);
+        }
+		// else there is no bonus
+        else{ 
+            Console.WriteLine("No bonus");
+        }
+    }
+}
