d972b43fd959012556d0b2aa9dba80eca82bba52
Zainab parveen <zainab.parveen_cs22@gla.ac.in>
2025-12-22 22:03:17 +0530

    [Zainab Parveen] check if a number is divisible by 5

---
+++ gcr-codebase/control-flow/level-1/DivisibleByFive.cs
@@ -0,0 +1,11 @@
+using System;
+class CheckDivisibilityByFive{
+    static void Main(){	
+		// take a number as input from user
+        int n = int.Parse(Console.ReadLine());
+		// check if the number is divible by 5 and store result in variable r
+        bool r = number % 5 == 0;
+		// print result
+        Console.WriteLine("Is number "+n+" divisible by 5? "+r);
+    }
+}
