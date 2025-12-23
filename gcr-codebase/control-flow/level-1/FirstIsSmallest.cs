89fd28bde4a46e704cd8d7de57dd175681d94b5f
zainabZP <122631115+zainabZP@users.noreply.github.com>
2025-12-23 18:56:04 +0530

    [Zainab Parveen] refactored. First Is Smallest?

--- gcr-codebase/control-flow/level-1/FirstIsSmallest.cs
+++ gcr-codebase/control-flow/level-1/FirstIsSmallest.cs
@@ -2,13 +2,12 @@ using System;
 class CheckIfFirstIsSmallest{
     static void Main(){
 		// taking 3 numbers as input from user
-        string[] input = Console.ReadLine().Split();
-        int n1 = int.Parse(input[0]);
-        int n2 = int.Parse(input[1]);
-        int n3 = int.Parse(input[2]);
-		// check if n1<n2 & n1<n3, and store the result (true or false) in a variable r
-        bool r = n1 < n2 && n1 < n3;
-		if(r)
+        string[] input=Console.ReadLine().Split();
+        int n1=int.Parse(input[0]);
+        int n2=int.Parse(input[1]);
+        int n3=int.Parse(input[2]);
+		// check if n1<n2 & n1<n3
+		if(n1<n2&&n1<n3)
 			Console.WriteLine("first number is smallest");
 		else{
 			Console.WriteLine("first number is not smallest");
