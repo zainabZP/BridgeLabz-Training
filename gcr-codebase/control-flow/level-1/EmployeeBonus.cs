9fdc7d8bca198c5792d4cd6f71986ba9d0d33a2c
zainabZP <122631115+zainabZP@users.noreply.github.com>
2025-12-23 18:48:37 +0530

    [Zainab Parveen] Employee Bonus

--- gcr-codebase/control-flow/level-1/EmployeeBonus.cs
+++ gcr-codebase/control-flow/level-1/EmployeeBonus.cs
@@ -2,12 +2,12 @@ using System;
 class BonusOfEmployees{	
     static void Main(){
 		// take salary and year as input from user
-        double salary = double.Parse(Console.ReadLine());
-        int y = int.Parse(Console.ReadLine());
+        double sal=double.Parse(Console.ReadLine());
+        int y=int.Parse(Console.ReadLine());
 		// if year > 5, bonus will be 5% of salary
 		if (y>5) {
-            double bonus = salary * 0.05;
-            Console.WriteLine("Bonus = " + bonus);
+            double bonus=sal*0.05;
+            Console.WriteLine("Bonus = "+bonus);
         }
 		// else there is no bonus
         else{ 
