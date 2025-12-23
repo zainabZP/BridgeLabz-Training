5016f97676bfc14ed989c862427621b42028bd53
zainabZP <122631115+zainabZP@users.noreply.github.com>
2025-12-23 18:46:59 +0530

    [Zainab Parveen] Check Number Sign

--- gcr-codebase/control-flow/level-1/CheckNumber.cs
+++ gcr-codebase/control-flow/level-1/CheckNumber.cs
@@ -1,15 +1,12 @@
 using System;
 class CheckSign{
-	
     static void Main(){
-		
 		// take a number as input from use
         int n=int.Parse(Console.ReadLine());
-		
 		// Conditions to check whether a number is >0, <0, or =0
-        if (n>0)
+        if(n>0)
             Console.WriteLine("positive");
-        else if (n<0)
+        else if(n<0)
             Console.WriteLine("negative");
         else
             Console.WriteLine("zero");
