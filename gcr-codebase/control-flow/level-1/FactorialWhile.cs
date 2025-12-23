2192d3916074099093f7365c3e64d2ebaa461853
zainabZP <122631115+zainabZP@users.noreply.github.com>
2025-12-23 18:53:40 +0530

    [Zainab Parveen] Factorial Using While Loop

--- gcr-codebase/control-flow/level-1/FactorialWhile.cs
+++ gcr-codebase/control-flow/level-1/FactorialWhile.cs
@@ -3,20 +3,12 @@ class FactorialWhile{
     static void Main(){
         // enter the number whose factorial to be calculated
         int n=int.Parse(Console.ReadLine());
-        // checks if n>1
-        if (n>=1){
-            // if n>1 calculate its factorial using while loop and store result in variable f;
-            int f = 1;
-            int i = 1;
-            while (i<=n){
-                f=f*i; i++;
-            }
-            // print factorial of the number
-            Console.WriteLine("Factorial = " +f );
+        int f=1;
+        int i=1;
+        while (i<=n){
+            f=f*i; i++;
         }
-        // if n<1, ask user to enter a positive integer
-        else{
-            Console.WriteLine("enter a positive integer");
-        }
+        // print factorial of the number
+        Console.WriteLine("Factorial = " +f);
     }
 }
