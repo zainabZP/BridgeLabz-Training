5d29c5b08b84ce55cfc19eee23a61cf0e57bc344
zainabZP <122631115+zainabZP@users.noreply.github.com>
2025-12-23 18:59:28 +0530

    [Zainab Parveen] Armstrong Number

--- gcr-codebase/control-flow/level-3/ArmstrongNumber.cs
+++ gcr-codebase/control-flow/level-3/ArmstrongNumber.cs
@@ -3,15 +3,16 @@ class ArmstrongNumber{
     static void Main(){
         Console.Write("Enter a number: ");
         int n=int.Parse(Console.ReadLine());
-        int ogNumber=n;
+        int temp=n;
         int sum=0;
         // loop until number becomes 0
         while (n!=0){
-            int t=n%10;      // get last digit
-            sum=sum+(t*t*t); // cube
+            int t=n%10; // get last digit
+            int cube=t*t*t;
+            sum=sum+cube; 
             n=n/10; // remove last digit
         }
-        if (sum==ogNumber)
+        if (sum==temp)
             Console.WriteLine("Armstrong Number");
         else
             Console.WriteLine("Not an Armstrong Number");
