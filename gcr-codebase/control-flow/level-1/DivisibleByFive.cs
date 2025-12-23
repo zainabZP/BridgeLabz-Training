d9f4a8e4b35be6f2679589441be5b8f1641c117d
zainabZP <122631115+zainabZP@users.noreply.github.com>
2025-12-23 18:41:09 +0530

    [Zainab Parveen] Divisible By Five

--- gcr-codebase/control-flow/level-1/DivisibleByFive.cs
+++ gcr-codebase/control-flow/level-1/DivisibleByFive.cs
@@ -2,9 +2,9 @@ using System;
 class CheckDivisibilityByFive{
     static void Main(){	
 		// take a number as input from user
-        int n = int.Parse(Console.ReadLine());
+        int n =int.Parse(Console.ReadLine());
 		// check if the number is divible by 5 and store result in variable r
-        bool r = number % 5 == 0;
+        bool r =n%5==0;
 		// print result
         Console.WriteLine("Is number "+n+" divisible by 5? "+r);
     }
