3ed6c36e2d0bd01c957be607f1e7412e694c08a0
Zainab parveen <zainab.parveen_cs22@gla.ac.in>
2026-01-21 11:00:17 +0530

    Remove dsa folder from collection branch

--- dsa-c-sharp-practice/gcr-codebase/c-sharp-searching/Append.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using System.Text;
-class AppendString{
-    public static void Main(){
-        StringBuilder sb=new StringBuilder(Console.ReadLine());
-        sb.Append(Console.ReadLine());
-        Console.WriteLine(sb.ToString());
-    }
-}
--- dsa-c-sharp-practice/gcr-codebase/c-sharp-searching/StringReverse.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using System.Text;
-class Reverse{
-    public static void Main(){
-        StringBuilder sb=new StringBuilder(Console.ReadLine());
-        string reversed = new string(sb.ToString().Reverse().ToArray()); // ToString converts obj to string, string class implements IEnumerable<char>, Reverse() is a LINQ method Returns IEnumerable<char>, The string constructor does NOT accept IEnumerable<char>. So we must convert the sequence to a char[].
-        Console.WriteLine(reversed);
-    }
-}
