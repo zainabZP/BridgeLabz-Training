a01d231da80e032f5a3b2577001469992daf6e92
Zainab parveen <zainab.parveen_cs22@gla.ac.in>
2026-01-29 22:42:22 +0530

    [Zainab Parveen] Bank Account testing

---
+++ collection-c-sharp-practice/scenario-based/BankAccount/BankAccountTest/Program.cs
@@ -0,0 +1,27 @@
+using System;
+
+public class Program
+{
+    public decimal Balance { get; private set; }
+
+    public Program(decimal initialBalance)
+    {
+        Balance = initialBalance;
+    }
+
+    public void Deposit(decimal amount)
+    {
+        if (amount < 0)
+            throw new Exception("Deposit amount cannot be negative");
+
+        Balance += amount;
+    }
+
+    public void Withdraw(decimal amount)
+    {
+        if (amount > Balance)
+            throw new Exception("Insufficient funds.");
+
+        Balance -= amount;
+    }
+}
